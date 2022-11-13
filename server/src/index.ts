0af7d6deacb0787cbc02d4483ef7976908d45499
[Taufiq Dhanial] <[taufiq.dhanil@gmail.com]>
2022-11-13 22:11:34 +0700

    basic express with mikro ORM

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,27 @@
 import { MikroORM, RequestContext } from "@mikro-orm/core";
+import express from "express";
 import { __prod__ } from "./constants";
-// import { Post } from "./entities/Post";
+import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
-  await RequestContext.createAsync(orm.em, async () => {
-    // const post = orm.em.create(Post, { title: "my new post" });
-    // console.log(post);
-    // await orm.em.persistAndFlush(post);
-    // const posts = await orm.em.find(Post, {});
-    // console.log(posts);
+
+  const app = express();
+
+  //mikro ORM RequestContext helper (https://mikro-orm.io/docs/identity-map)
+  app.use((_req, _res, next) => {
+    RequestContext.create(orm.em, next);
+  });
+
+  app.get("/", async (_req, res) => {
+    const posts = await orm.em.find(Post, {});
+    res.send(posts);
+  });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
   });
 };
 
