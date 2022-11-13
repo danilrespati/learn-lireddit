fa635c94b32d3620eb773a11d74e0bf1f6dfc034
[Taufiq Dhanial] <[taufiq.dhanil@gmail.com]>
2022-11-13 21:41:37 +0700

    mikroORM setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello world!");
+import { MikroORM, RequestContext } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+// import { Post } from "./entities/Post";
+import mikroConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up();
+  await RequestContext.createAsync(orm.em, async () => {
+    // const post = orm.em.create(Post, { title: "my new post" });
+    // console.log(post);
+    // await orm.em.persistAndFlush(post);
+    // const posts = await orm.em.find(Post, {});
+    // console.log(posts);
+  });
+};
+
+main().catch((err) => {
+  console.error(err);
+});
